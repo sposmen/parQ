f1b4da89cb1cbf8401af56f3369bee4d0e6a0b68
Roger Padilla <rpadilla@velocitypartners.net>
2018-09-03 10:34:50 -0500

    home cmp

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,1 +1,26 @@
 import './main.style';
+
+import { router } from './shared/router';
+import { sendHtml } from './shared/utils/dom.util';
+import { HomeCmp } from './home/home.cmp';
+
+router
+  .use('/', async (req, res, next) => {
+    if (req.listening) {
+      const html = HomeCmp();
+      sendHtml(html);
+    }
+    next();
+  })
+  .listen();
+
+
+window.fbAsyncInit = () => {
+  FB.init({
+    appId: '195322843827243',
+    cookie: false,
+    xfbml: false,
+    version: 'v3.1'
+  });
+};
+
