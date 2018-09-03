190ec563590e466eee9a34590134a61ac11d983e
Danny Goncalves <dgoncalves@velocitypartners.net>
2018-09-03 15:48:02 -0500

    Changed styles for the dashboard

--- src/client/dashboard/dashboard.cmp.ts
+++ src/client/dashboard/dashboard.cmp.ts
@@ -2,25 +2,24 @@ export function DashboardCmp(assigns: any) {
   return `
     <div class="dashboard card-list">
       <h1>Dashboard</h1>
-      <table>
-        <tr>
-          <th>Name</th>
-          <th>Plate</th>
-          <th>Slot</th>
-        </tr>
         ${renderAssigns(assigns)}
-      </table>
     </div>
   `;
 }
 
 function renderAssigns(assigns: any) {
   return assigns.map((assign: any) => {
-    return `<tr class="card-it">
-      <td>${assign.name}</td>
-      <td>${assign.plate}</td>
-      <td>${assign.slot < 9 ? '0' + assign.slot : assign.slot}</td>
-      </tr>`;
+    return `<div class="card-it">
+        <div class="dashboard-card-left">
+          <p><b>User: </b>${assign.name}</p>
+          <p><b>Plate: </b>${assign.plate}</p>
+          <p><b>Assigned cell: </b>${assign.slot < 9 ? '0' + assign.slot : assign.slot}</p>
+          <p><b>Car model: </b>${assign.model}</p>
+        </div>
+        <div class="dashboard-card-right">
+          <button class="btn">Release</button>
+        </div>
+      </div>`;
   }
   ).join('\n');
 }
